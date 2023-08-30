ce60894587a8266fff8f7990a5d5e7d1d68bd42d
samau3 <samau415@gmail.com>
2023-08-29 20:16:48 -0700

    Update entry to target proper index file name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.jsx"),
+      entry: resolve(__dirname, "src/index.js"),
       name: "shadow-ui",
 
       fileName: "index",
