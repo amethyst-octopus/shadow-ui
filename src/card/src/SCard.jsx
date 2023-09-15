96ccc4b89a2467a7f60c29ca507f282d62c53569
Alex Linden <alextl2004@gmail.com>
2023-09-15 13:03:51 -0700

    card with styling

--- src/card/src/SCard.jsx
+++ src/card/src/SCard.jsx
@@ -1,25 +1,52 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { startingHexColor, colorToShadows } from "../../utils";
 
-export default function Card({bgColor=startingHexColor(), media, text}){
-    const [backgroundColor, setBackgroundColor] = useState(bgColor)
-    const [shadows, setShadows] = useState({})
+export default function Card({ bgColor = startingHexColor(), media, text }) {
+    const [backgroundColor, setBackgroundColor] = useState(bgColor);
+    const [shadows, setShadows] = useState({});
 
-    useEffect(() =>{
-        setShadows(colorToShadows(backgroundColor))
-    }, [backgroundColor])
+    useEffect(() => {
+        setShadows(colorToShadows(backgroundColor));
+    }, [backgroundColor]);
 
-    /**will need to update these number to be more dynamic */
-    let style = `border-radius: 50px;
-    background: ${backgroundColor};
-    box-shadow:  18px 18px 36px ${shadows.darkShadow},
-                 -18px -18px 36px ${shadows.lightShadow};`
 
-    return (
-        <div style={style}>
-            <img src={media}></img>
-            <p>{text}</p>
-        </div>
-    )
+    let style = {
+        margin: "2rem",
+        maxWidth: "30%",
+        display: "flex",
+        overflow: "hidden",
+        flexDirection: "column",
+        borderRadius: "50px",
+        background: backgroundColor,
+        boxShadow: `20px 20px 40px ${shadows.darkShadow},
+                 -20px -20px 40px ${shadows.lightShadow}`
+
+    };
+
+    let mediaStyle = {
+        borderRadius: "50px",
+        padding: "1rem",
+        paddingBottom: "0.2rem",
+        // maxWidth: "100px"
+        maxHeight: "33.33%"
+    };
+
+    let textStyle = {
+        paddingRight: "1rem",
+        paddingLeft: "1rem"
+    };
+    if (media) {
+        return (
+            <div style={style}>
+                <img style={mediaStyle} src={media}></img>
+                <p style={textStyle}>{text}</p>
+            </div>
+        );
+    } else {
+        return (
+            <div style={style}>
+                <p style={textStyle}>{text}</p>
+            </div>
+        );
+    }
 }
-
