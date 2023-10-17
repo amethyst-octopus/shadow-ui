e5c50d996f3cfb31dbfa5f4b6776c13a21f63cef
Ashaun Thomas <ashaunthomas@siu.edu>
2023-10-17 11:56:34 -0700

    updates button styles and stories

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A neumorphic component design system
 ## Local Development Workflow
 
 1. Run `npm run dev` to start Storybook. Check the terminal for remaining instructions
+
+## Special Thanks
+
+- Christophe de Canteloube for inspiration via Codepen neumorphism examples
--- src/button/src/T1Button.jsx
+++ src/button/src/T1Button.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import './Button.css';
 
-export const T1Button = (props) => (
-  <button className={`button ${props.kind}`} {...props}>
-    {props.text}
-  </button>
-);
+export const T1Button = ({text, onPress, style, additionalDirectives}) => {
+
+  const [isFocused, setFocus] = useState(false);
+
+  const handleClick = (e) => {
+    setFocus(true);
+    if (onPress)
+      onPress();
+  }
+
+  return (
+    <button
+      onMouseLeave={() => setFocus(false)}
+      onFocus={() => setFocus(true)}
+      onBlur={() => setFocus(false)}
+      onClick={handleClick}
+      tabIndex={0} 
+      className={isFocused ? 'button button-focus' : 'button'}
+      style={style}
+      {...additionalDirectives}>
+      {text}
+    </button>
+  );
+};
