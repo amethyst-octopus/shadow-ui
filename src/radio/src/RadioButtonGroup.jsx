1f741615a2ba8f7961990df11054d7037148d708
Paige Godfrey <paagodfrey@gmail.com>
2023-10-18 20:17:52 -0700

    moved state to RadioButtonGroup

--- src/radio/src/RadioButtonGroup.jsx
+++ src/radio/src/RadioButtonGroup.jsx
@@ -1,24 +1,18 @@
 import { useState } from 'react';
 import './RadioButtonGroup.css';
 
-export const RadioButton = ({ id, name, value, label }) => {
-
-  const [isSelected, setSelected] = useState(null);
-
-  const onOptionChange = e => {
-    setSelected(e.target.value);
-  }
+export const RadioButton = ({ id, name, value, selected, label, onOptionChange }) => {
 
   return (
-    <div className="radio-button">
+    <div className="radio-button-wrapper">
       <input
         className="radio-state"
         type="radio"
         id={id}
         name={name}
         value={value}
-        checked={isSelected === value}
         onChange={onOptionChange}
+        checked={selected === value}
       />
       <label htmlFor={id}>{label}</label>
     </div>
@@ -26,16 +20,22 @@ export const RadioButton = ({ id, name, value, label }) => {
 }
 
 export const T1RadioButtonGroup = ({ name, options }) => {
+  const [selected, setSelected] = useState(null);
+
+  const onOptionChange = (e) => {
+    setSelected(e.target.value);
+  }
+
   return (
     <div className="radio-button-group">
       {
-        options.map(({ id, value, label }) => (
+        options.map((option) => (
           <RadioButton
-            id={id}
-            value={value}
             name={name}
-            label={label}
-            key={label}
+            selected={selected}
+            key={option.label}
+            onOptionChange={onOptionChange}
+            {...option}
           ></RadioButton>
         ))
       }
