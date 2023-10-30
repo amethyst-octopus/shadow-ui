43cf7115d164bbf5a969d7d1a71bee56e0aaadc9
Ashaun Thomas <ashaunthomas@siu.edu>
2023-10-30 00:25:36 -0700

    Adds space in story title; removes name from controls

--- src/radio/src/RadioButtonGroup.stories.jsx
+++ src/radio/src/RadioButtonGroup.stories.jsx
@@ -1,17 +1,16 @@
 import { T1RadioButtonGroup } from "./RadioButtonGroup";
 
 export default {
-  title: 'Components/RadioButtons',
-  component: T1RadioButtonGroup
-}
+	title: "Components/Radio Buttons",
+	component: T1RadioButtonGroup,
+};
 
 export const Type_One = {
-  args: {
-    name: "test",
-    options: [
-      { id: "opt1", label: "Option 1", value: "opt1" },
-      { id: "opt2", label: "Option 2", value: "opt2" },
-      { id: "opt3", label: "Option 3", value: "opt3" },
-    ],
-  }
-}
+	args: {
+		options: [
+			{ id: "opt1", label: "Option 1", value: "opt1" },
+			{ id: "opt2", label: "Option 2", value: "opt2" },
+			{ id: "opt3", label: "Option 3", value: "opt3" },
+		],
+	},
+};
