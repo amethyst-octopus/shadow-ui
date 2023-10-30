4425745883a8b773719b65262b33c8289d7972d5
Ashaun Thomas <ashaunthomas@siu.edu>
2023-10-30 02:28:58 -0700

    adds card story; comments out color utils and sets todo

--- src/card/src/Card.stories.jsx
+++ src/card/src/Card.stories.jsx
@@ -1,7 +1,12 @@
-import { T1Card } from "./T1Card"
+import { T1Card } from "./T1Card";
 
 export default {
 	component: T1Card,
-	title: 'Components/Cards'
-}
+	title: "Components/Cards",
+};
 
+export const Type_One = {
+	args: {
+		children: "Hello this is a card",
+	},
+};
