ce036c957be79c32e5c38566807ef8ac3592ed67
Ashaun Thomas <ashaunthomas@siu.edu>
2023-10-30 14:43:41 -0700

    adds prod build

---
+++ storybook-static/assets/Introduction-28e75693.js
@@ -0,0 +1,199 @@
+import{j as t,a as e,F as s}from"./jsx-runtime-29545a09.js";import{M as d}from"./index-410b2115.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-e01ad46f.js";import"../sb-preview/runtime.js";import"./chunk-6P7RB4HF-c879cb99.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const n=()=>{const i=Object.assign({svg:"svg",path:"path"},a());return t(i.svg,{viewBox:"0 0 14 14",width:"8px",height:"14px",style:{marginLeft:"4px",display:"inline-block",shapeRendering:"inherit",verticalAlign:"middle",fill:"currentColor","path fill":"currentColor"},children:t(i.path,{d:"m11.1 7.35-5.5 5.5a.5.5 0 0 1-.7-.7L10.04 7 4.9 1.85a.5.5 0 1 1 .7-.7l5.5 5.5c.2.2.2.5 0 .7Z"})})};function r(i){const o=Object.assign({h1:"h1",p:"p",strong:"strong"},a(),i.components);return e(s,{children:[t(d,{title:"Introduction"}),`
+`,e("div",{className:"sb-container",children:[e("div",{className:"sb-section-title",children:[t(o.h1,{id:"shadow-ui-a-neumorphic-design-system-for-react",children:"Shadow-UI: A Neumorphic Design System for React"}),t(o.p,{children:"A component library created with accessibility and usability in mind."})]}),e("div",{className:"sb-section",children:[e("div",{className:"sb-section-item",children:[t("svg",{width:"64px",height:"64px",strokeWidth:"1.5",viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",color:"#000000",children:t("path",{d:"M5 13l4 4L19 7",stroke:"#000000",strokeWidth:"1.5",strokeLinecap:"round",strokeLinejoin:"round"})}),t("h4",{className:"sb-section-item-heading",children:"Easy to use"}),e("p",{className:"sb-section-item-paragraph",children:["Our components are made to be used out of the box ",t(o.strong,{children:"or"})," modified to fit more niche use-cases.",t("br",{}),t("br",{})]}),e("a",{href:"https://github.com/amethyst-octopus/shadow-ui",target:"_blank",children:["Check out the tutorial",t(n,{})]})]}),e("div",{className:"sb-section-item",children:[t("svg",{width:"64px",height:"64px",strokeWidth:"1.5",viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",color:"#000000",children:t("path",{d:"M5 13l4 4L19 7",stroke:"#000000",strokeWidth:"1.5",strokeLinecap:"round",strokeLinejoin:"round"})}),t("h4",{className:"sb-section-item-heading",children:"Easy to configure"}),t("p",{className:"sb-section-item-paragraph",children:"Our documentation has built-in code snippet generation which allows you to copy/paste components straight from our site into your IDE"}),e("a",{href:"https://github.com/amethyst-octopus/shadow-ui",target:"_blank",children:["Check out a component",t(n,{})]})]}),e("div",{className:"sb-section-item",children:[t("svg",{width:"64px",height:"64px",strokeWidth:"1.5",viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",color:"#000000",children:t("path",{d:"M5 13l4 4L19 7",stroke:"#000000",strokeWidth:"1.5",strokeLinecap:"round",strokeLinejoin:"round"})}),e("div",{children:[t("h4",{className:"sb-section-item-heading",children:"Easy to contribute"}),t("p",{className:"sb-section-item-paragraph",children:"Proud of a component you modified for your use case? Want a component sooner than the core team can deliver it? Just make a PR to our Github repo"}),e("a",{href:"https://github.com/amethyst-octopus/shadow-ui",target:"_blank",children:["Star on GitHub",t(n,{})]})]})]})]})]}),`
+`,t("style",{children:`
+  .sb-container {
+    margin-bottom: 48px;
+  }
+
+  .sb-container > * {
+    margin: 0 auto;
+  }
+
+  .sb-section-item > div {
+    flex-grow: 2;
+  }
+  
+
+  svg {     
+    margin: 0 auto;
+    padding: 70px 0;  
+  }
+
+  .sb-section-item > svg {
+    transform: translateY(25px)
+  }
+
+
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .sb-section {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+
+  img {
+    object-fit: cover;
+  }
+
+  .sb-section-title {
+    margin-bottom: 32px;
+  }
+
+  .sb-section a:not(h1 a, h2 a, h3 a) {
+    font-size: 14px;
+  }
+
+  .sb-section-item {
+    border: 1px solid rgba(0, 0, 0, .15);
+    -webkit-background-clip: padding-box; /* for Safari */
+    background-clip: padding-box;
+    border-radius: 15px;
+    padding: 0px 30px;
+    display: flex;
+  }
+
+  .sb-section-item, .sb-grid-item {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .sb-section-item-heading {
+    padding-top: 20px !important;
+    padding-bottom: 5px !important;
+    margin: 0 !important;
+  }
+  .sb-section-item-paragraph {
+    margin: 0;
+    padding-bottom: 10px;
+  }
+
+  .sb-chevron {
+    margin-left: 5px;
+  }
+
+  .sb-features-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 32px 20px;
+  }
+
+  .sb-socials {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  .sb-socials p {
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 14px !important;
+  }
+
+  .sb-explore-image {
+    max-height: 32px;
+    align-self: flex-start;
+  }
+
+  .sb-addon {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    position: relative;
+    background-color: #EEF3F8;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    background: #EEF3F8;
+    height: 180px;
+    margin-bottom: 48px;
+    overflow: hidden;
+  }
+
+  .sb-addon-text {
+    padding-left: 48px;
+    max-width: 240px;
+  }
+
+  h4 {
+    font-size: 24px !important;
+  }
+
+  .sb-addon-text h4 {
+    padding-top: 0px;
+  }
+
+  .sb-addon-img {
+    position: absolute;
+    left: 345px;
+    top: 0;
+    height: 100%;
+    width: 200%;
+    overflow: hidden;
+  }
+
+  .sb-addon-img img {
+    width: 650px;
+    transform: rotate(-15deg);
+    margin-left: 40px;
+    margin-top: -72px;
+    box-shadow: 0 0 1px rgba(255, 255, 255, 0);
+    backface-visibility: hidden;
+  }
+
+  @media screen and (max-width: 800px) {
+    .sb-addon-img {
+      left: 300px;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .sb-section {
+      flex-direction: column;
+    }
+
+    .sb-features-grid {
+      grid-template-columns: repeat(1, 1fr);
+    }
+
+    .sb-socials {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    .sb-addon {
+      height: 280px;
+      align-items: flex-start;
+      padding-top: 32px;
+      overflow: hidden;
+    }
+
+    .sb-addon-text {
+      padding-left: 24px;
+    }
+
+    .sb-addon-img {
+      right: 0;
+      left: 0;
+      top: 130px;
+      bottom: 0;
+      overflow: hidden;
+      height: auto;
+      width: 124%;
+    }
+
+    .sb-addon-img img {
+      width: 1200px;
+      transform: rotate(-12deg);
+      margin-left: 0;
+      margin-top: 48px;
+      margin-bottom: -40px;
+      margin-left: -24px;
+    }
+  }
+  `})]})}function v(i={}){const{wrapper:o}=Object.assign({},a(),i.components);return o?t(o,Object.assign({},i,{children:t(r,i)})):r(i)}export{n as RightArrow,v as default};
+//# sourceMappingURL=Introduction-28e75693.js.map
