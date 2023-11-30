48c9292c9c165f228eac1c8c9d36c27514950d86
Alex Linden <alextl2004@gmail.com>
2023-11-29 22:46:45 -0800

    track and thumb are aligned

--- src/slider/src/Slider.jsx
+++ src/slider/src/Slider.jsx
@@ -1,63 +1,53 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Slider.css";
 import { v4 as uuidv4 } from 'uuid';
 
-export const Slider = ({ defaultValue=50, onClick, minValue=0, maxValue=100, onDrag, onChange }) => {
+export const Slider = ({ defaultValue = 50, minValue = 0, maxValue = 100, step }) => {
     const [value, setValue] = useState(defaultValue);
-    const [trackWidth, setTrackWidth] = useState(10)
-    const [startXTrack, setStartXTrack] = useState()
+    const [trackWidth, setTrackWidth] = useState(10);
+    const [startXTrack, setStartXTrack] = useState();
+    const [sliderRange, setSliderRange] = useState(value);
+    const [inputValue, setInputValue] = useState(value);
+    const sliderRef = useRef(null);
 
-    const elID = uuidv4()
+    function handleSliderInput() {
+        //Get range between max & min values
+        const range = maxValue - minValue;
+        //Get distance between value and min value
+        const distance = sliderRef.current.value - minValue;
+
+        const percentage = (distance / range) * 100;
+        setSliderRange(percentage);
+        setInputValue(sliderRef.current.value);
+    }
 
     useEffect(() => {
-        const sliderEl = document.getElementById(elID);
-        const rect = sliderEl.getBoundingClientRect();
-        setStartXTrack(rect.left)
-        setTrackWidth(rect.left + defaultValue)
-        console.log(sliderEl.children)
-        console.log(rect)
-        window.addEventListener('mousemove', function(event) {
-            var x = event.clientX;
-            var y = event.clientY;
-            // console.log('The mouse is at X: ' + x + ' and Y: ' + y);
-          });
-    }, [])
+        handleSliderInput();
+    }, [sliderRef]);
 
-    const handleClick = (e) => {
-        // console.log(typeof parseInt(e.target.value));
-        if (onClick) onClick(e);
-        setValue(parseInt(e.target.value));
-        setTrackWidth(value + 12)
-        // setTrackWidth((parseInt(e.target.value)-minValue)/(maxValue-minValue))
-    };
-
-    const handleDrag = (e) => {
-        if (onDrag) onDrag(e)
-        // setValue(parseInt(e.target.value))
-    }
-
-    const handleChange = (e) =>{
-        if(onChange) onChange(e)
-        setValue(parseInt(e.target.value))
-        console.log('trackwidth: ' + trackWidth);
-        // console.log(e.ClientX)
-        setTrackWidth(value + 16)
-        // console.log('value:' + value);
-    }
+    const elID = uuidv4();
 
     return (
         <>
             <div className="sui-slider" id={elID}>
-                <div className="sui-slider-track-progress" style={{ width: trackWidth + 'px' }}></div>
+                {/* <div className="sui-slider-track-progress" style={{ width: trackWidth + 'px' }}></div> */}
                 <input
                     className="sui-slider-input"
                     type="range"
                     min={minValue}
                     max={maxValue}
                     value={value}
-                    onChange={handleChange}
-                    onClick={handleClick}
-                    onDrag={handleDrag} />
+                    onInput={handleSliderInput}
+                    ref={sliderRef}
+                    step={step} />
+                <div
+                    className="slider-thumb"
+                    style={{ left: `calc(${sliderRange}% - 0.5em)` }}
+                ></div>
+                <div
+                    className="sui-slider-track-progress"
+                    style={{ width: `${sliderRange}%` }}
+                ></div >
             </div>
 
         </>
