b8b838f5af1afde83409b8278e2636985b603b93
Alex Linden <90168115+Alex-Linden@users.noreply.github.com>
2024-01-23 03:22:37 -0800

    Slider style (#59)
    * add handleChange prop
    
    * WIP styling
    
    * Update slider style to match swtich
    
    removed boarder
    added handle change fucntion

--- src/slider/src/Slider.jsx
+++ src/slider/src/Slider.jsx
@@ -2,27 +2,30 @@ import React, { useState, useEffect, useRef } from "react";
 import "./Slider.css";
 import { v4 as uuidv4 } from 'uuid';
 
-export const Slider = ({ defaultValue = 50, minValue = 0, maxValue = 100, step }) => {
-    // const [value, setValue] = useState(defaultValue);
+export const Slider = ({ defaultValue = 50, minValue = 0, maxValue = 100, step, handleChange }) => {
     const [sliderRange, setSliderRange] = useState(defaultValue);
     const [inputValue, setInputValue] = useState(defaultValue);
     const sliderRef = useRef(null);
 
     function handleSliderInput() {
-        //Get range between max & min values
+        // Get range between max & min values
         const range = maxValue - minValue;
-        //Get distance between value and min value
+        // Get distance between value and min value
         const distance = sliderRef.current.value - minValue;
 
         let percentage = (distance / range) * 100;
-        //Conditional to handle thumb overflowing from track border
-        if(percentage < 3.5){
-            percentage = 3.5
-        } else if(percentage > 98){
-            percentage = 98
+        // Conditional to handle thumb overflowing from track border
+        if (percentage < 2.9) {
+            percentage = 2.9;
+        } else if (percentage > 99.4) {
+            percentage = 99.4;
         }
         setSliderRange(percentage);
         setInputValue(sliderRef.current.value);
+        console.log(sliderRange)
+        if (handleChange) {
+            handleChange(sliderRef.current.value);
+        }
     }
 
     useEffect(() => {
@@ -34,7 +37,6 @@ export const Slider = ({ defaultValue = 50, minValue = 0, maxValue = 100, step }
     return (
         <>
             <div className="sui-slider" id={elID}>
-                {/* <div className="sui-slider-track-progress" style={{ width: trackWidth + 'px' }}></div> */}
                 <input
                     className="sui-slider-input"
                     type="range"
@@ -48,10 +50,12 @@ export const Slider = ({ defaultValue = 50, minValue = 0, maxValue = 100, step }
                     className="slider-thumb"
                     style={{ left: `calc(${sliderRange}% - 0.75em)` }}
                 ></div>
-                <div
-                    className="sui-slider-track-progress"
-                    style={{ width: `calc(${sliderRange}% - 0.5em)` }}
-                ></div >
+                {sliderRange > 2.9 ?
+                    <div
+                        className="sui-slider-track-progress"
+                        style={{ width: `calc(${sliderRange}% - 0.5em)` }}
+                    ></div >
+                    : null}
             </div>
 
         </>
